d3b8e4f44cf7589e6bb6240d9c201b4271b166c3
liziwl <leezisy@gmail.com>
2018-04-26 22:55:20 +0800

    finished task2

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -88,6 +88,7 @@ struct thread
     char name[16];                      /* Name (for debugging purposes). */
     uint8_t *stack;                     /* Saved stack pointer. */
     int priority;                       /* Priority. */
+    int time_slice;                     /* Time slice.*/
     int ticks_blocked;                  /*Number of ticks to sleep*/ 
     
     struct list_elem allelem;           /* List element for all threads list. */
@@ -138,6 +139,7 @@ void thread_foreach (thread_action_func *, void *);
 
 int thread_get_priority (void);
 void thread_set_priority (int);
+int thread_get_time_slice (void);
 
 int thread_get_nice (void);
 void thread_set_nice (int);
